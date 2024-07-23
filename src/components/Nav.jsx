81499a0dad3b6939f5fac0d268c42be8b4056783
Ítalo Emídio <italo.emidio7@gmail.com>
2024-07-23 11:54:13 -0300

    feat: adicionando alterações

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,34 +1,62 @@
-import logodigital from "/logo-digital-college.png"
-import './Nav.css'
-import iconecarrinho from '/icone-carrinho.png'
-export default function Nav () {
-    return (
-        <div>
-            <div className="nav">
-                <div className="nav-1">
-                    <div>
-                        <img className="image-logo" src= {logodigital} alt="Logo da Digital College" />
-                    </div>
-                    <div>
-                        <input id="search" type="text" placeholder="Pesquisar produto..." />
-                    </div>
-                    <div className="buttons">
-                        <button className="register-button"><span className="text-button">Cadastre-se</span></button>
-                        <button className="enter-button"><span className="text-button-2">Entrar</span></button>
-                    </div>
-                    <div className="icon-carrinho">
-                        <img className="icone-carrinho" src= {iconecarrinho} alt="icone de carrinho" />
-                    </div>
-                </div>
-                <div className="menu">
-                    <ul className="menu-lista">
-                        <li><a className="options" href=""></a>Home</li>
-                        <li><a className="options" href=""></a>Produtos</li>
-                        <li><a className="options" href=""></a>Categorias</li>
-                        <li><a className="options" href=""></a>Meus Pedidos</li>
-                    </ul>
-                </div>
-            </div>
-        </div>    
-    )
+import logodigital from "/logo-digital-college.png";
+import "./Nav.css";
+import iconecarrinho from "/icone-carrinho.png";
+import search from "/search.png";
+import { useState } from "react";
+export default function Nav() {
+  return (
+    <div>
+      <div className="nav">
+        <div className="nav-1">
+          <div>
+            <img
+              className="image-logo"
+              src={logodigital}
+              alt="Logo da Digital College"
+            />
+          </div>
+          
+          <div className="search-container">
+            <input id="search" type="text" placeholder="Pesquisar produto..." />
+            <button className="button-search" type="submit">
+              <img src={search} alt="lupa de pesquisa" />
+            </button>
+        </div>
+
+
+          <div className="buttons">
+            <button className="register-button">
+              <span className="text-button">Cadastre-se</span>
+            </button>
+            <button className="enter-button">
+              <span className="text-button-2">Entrar</span>
+            </button>
+          </div>
+          <div className="icon-carrinho" onClick={() => setCarrinho([...carrinho, 'novo item'])}>
+            <img
+              className="icone-carrinho"
+              src={iconecarrinho}
+              alt="icone de carrinho"
+            />
+          </div>
+        </div>
+        <div className="menu">
+          <ul className="menu-lista">
+            <li>
+              <a className="options" href=""></a>Home
+            </li>
+            <li>
+              <a className="options" href=""></a>Produtos
+            </li>
+            <li>
+              <a className="options" href=""></a>Categorias
+            </li>
+            <li>
+              <a className="options" href=""></a>Meus Pedidos
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
