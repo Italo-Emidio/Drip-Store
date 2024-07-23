396339449358a8551bdc5d0f040cd8c9fa205b7d
FABRICIO ALMEIDA DIAS PINHEIRO <FABRICIO6174@DIGITALCOLLEGE.LOCAL>
2024-07-23 08:19:45 -0300

    ajustei o dnv

--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -7,7 +7,7 @@ export default function ProductListing() {
     <div className="div-daddy">
         <div className="titulo-product">
             <h2 className="product-em-altas">Produtos em alta</h2>
-            <p className="verprodutos">Ver Produtos <img className="setinha" src={seta} /></p>
+            <p className="verprodutos" style={{color: '#c92071'}}>Ver Produtos <img className="setinha" src={seta} /></p>
         </div>
         <div className="container-card">
             
